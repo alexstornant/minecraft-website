2044175e58ce406b028a535e49f911069c7c7797
alexstornant <stornant.alex@gmail.com>
2022-06-18 23:07:30 -0400

    removed sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MaterialModule} from "../material/material.module";
+import { MaterialModule } from "../material/material.module";
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
